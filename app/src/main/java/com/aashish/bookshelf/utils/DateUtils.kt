712d0c380c2581b226e05513306df547b6d6559d
aashish-ranjan <aashishranjan2011@gmail.com>
2023-10-28 14:59:42 +0530

    fix dateUtil epochToYr conversion error

--- app/src/main/java/com/aashish/bookshelf/utils/DateUtils.kt
+++ app/src/main/java/com/aashish/bookshelf/utils/DateUtils.kt
@@ -2,20 +2,22 @@ package com.aashish.bookshelf.utils
 
 import android.os.Build
 import java.time.Instant
+import java.time.ZoneId
+import java.time.ZonedDateTime
 import java.util.Calendar
 
 object DateUtils {
 
-    fun epochToYear(epochMillis: Long): Int {
+    fun epochToYear(epochInSeconds: Long): Int {
         return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             // For API level >= 26, use java.time API
-            val instant = Instant.ofEpochMilli(epochMillis)
-            val zonedDateTime = java.time.ZonedDateTime.ofInstant(instant, java.time.ZoneId.systemDefault())
+            val instant = Instant.ofEpochSecond(epochInSeconds)
+            val zonedDateTime = ZonedDateTime.ofInstant(instant, ZoneId.systemDefault())
             zonedDateTime.year
         } else {
             // For API level < 26, use Calendar
             val calendar = Calendar.getInstance()
-            calendar.timeInMillis = epochMillis
+            calendar.timeInMillis = epochInSeconds * 1000
             calendar.get(Calendar.YEAR)
         }
     }
